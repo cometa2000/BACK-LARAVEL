0b2129985640e2384d1ea586e9b8374ea5be97db
cometa2000 <tuxmorro2016@gmail.com>
2025-09-07 04:57:35 -0600

    final de roles y permisos

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RolePermissionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,4 +32,10 @@ Route::group([
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
     Route::post('/me', [AuthController::class, 'me'])->name('me');
+});
+
+Route::group([
+    'middleware' => 'auth:api',
+], function ($router) {
+    Route::resource("roles",RolePermissionController::class); 
 });
